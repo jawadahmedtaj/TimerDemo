43940431d378d0f23d136c33058867f89a6b5a4a
jawadahmed <jawadahmedtaj@gmail.com>
2019-03-24 21:57:42 +0100

    Completed

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.timer;
 
+import android.net.IpSecManager;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        final Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                //Insert code to be run every second
+                System.out.println("Runnable has run, and a second must have passed...");
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(run);
+        */
+
+        new CountDownTimer(10000,1000){
+          public void onTick(long milliSecondsUntilDone){
+              //Countdown is couting down
+              System.out.println("Seconds left" + String.valueOf(milliSecondsUntilDone/1000));
+          }
+          public void onFinish(){
+              //Counter is finished after 10 seconds
+              System.out.println("Done! Countdown timer finished");
+          }
+        }.start();
     }
 }
